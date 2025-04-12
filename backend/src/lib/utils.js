1a832c50251950f1f30baec4915534521549db25
YUBARAJ MAHANTA <yubrajmahanta@gmail.com>
2025-04-12 15:46:17 +0530

    Update utils.js
    this   t0  =process.env.NODE_ENV !== "developement"
    this  true

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateTokens = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, //ms
     httpOnly: true, //prevent XSS attacks (cross-site scripting)
-    secure: process.env.NODE_ENV !== "developement", // ensure secure cookies in production
+    secure: true, // ensure secure cookies in production
   });
 
   return token;
